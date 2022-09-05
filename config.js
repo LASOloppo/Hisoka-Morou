ee36c51960d3943d99f49c2d7e69e3d88c50337d
LASOloppo <111619867+LASOloppo@users.noreply.github.com>
2022-09-05 15:22:21 +0800

    Update config.js

--- config.js
+++ config.js
@@ -18,10 +18,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
+global.owner = ['6289510402229','6289510402229','6289510402229','6289510402229']
+global.premium = ['6289510402229']
 global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
+global.author = 'Atsair Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
